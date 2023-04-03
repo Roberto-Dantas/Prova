a46a8b594e2856f6d55e2ae69029a871ff4e17ad
Ro005 <84298912+Roberto-Dantas@users.noreply.github.com>
2023-04-03 08:05:37 -0300

    Add files via upload

---
+++ data/question.js
@@ -0,0 +1,134 @@
+export const portugueseQuestions = [{ 
+        question:"Qual das seguintes palavras está correta:",
+        correct: "02",
+        alt1:"Asterístico",
+        alt2:"Asteristico",
+        alt3:"Asterixtico",
+        alt4:"Asteristisco",
+        alt5:"Asteristíco"
+    },{
+        question:"Qual dos seguintes verbos está no Imperativo:",
+        correct: "03",
+        alt1:"Beber",
+        alt2:"Voei",
+        alt3:"Cante",
+        alt4:"Chegarei",
+        alt5:"Andando"
+    },{
+        question:"O Romance 'O Cortiço' foi escrito por quem?",
+        correct: "05",
+        alt1:"Clarice Lispector",
+        alt2:"Machado de Assis",
+        alt3:"J. K. Rowling",
+        alt4:"Graciliano Ramos",
+        alt5:"Aluísio de Azevedo"
+    },{
+        question:"Alentar é o mesmo que...",
+        correct: "04",
+        alt1:"reduzir, diminuir",
+        alt2:"andar, caminhar",
+        alt3:"cessar, parar",
+        alt4:"animar, encorajar",
+        alt5:"tolerar, aguentar"
+    },{
+        question:"Pandemônio tem relação com:",
+        correct: "01",
+        alt1:"confusão",
+        alt2:"diabo",
+        alt3:"fabricação de pães",
+        alt4:"epidemia",
+        alt5:"tecnologia"
+    },{
+        question:"A Geórgia é um país localizado na interseção da Europa com a Ásia. Quem nasce na Geórgia é:",
+        correct: "01",
+        alt1:"georgiano",
+        alt2:"geórgio",
+        alt3:"georgino",
+        alt4:"georgete",
+        alt5:"georgiense"
+    }]
+    
+export const mathematicsQuestions = [{ 
+        question:"Qual desses não foi um Matemático?",
+        correct: "03",
+        alt1:"Isaac Newton",
+        alt2:"Pitágoras",
+        alt3:"Sócrates",
+        alt4:"Leonhard Paul",
+        alt5:"Arquimedes"
+    },{
+        question:"Um comércio do interior cobra de seus clientes, quando pagam em atraso, juros simples de 1% ao mês.<br><br>Nessa condição, qual foi o tempo de atraso de uma prestação de R$ 1.250,00 se esta foi quitada com R$ 1.350,00?",
+        correct: "02",
+        alt1:"9 meses",
+        alt2:"8 meses",
+        alt3:"10 meses",
+        alt4:"9,5 meses",
+        alt5:"10,33 meses"
+    },{
+        question:"Qual é a raiz quadrada de 484:",
+        correct: "04",
+        alt1:"25",
+        alt2:"20",
+        alt3:"19",
+        alt4:"22",
+        alt5:"21.5"
+    },{
+        question:"Sabe-se que em todo triângulo a medida de cada lado é diretamente proporcional ao seno do ângulo oposto ao lado.<br><br>Usando essa informação, conclui-se que a medida do lado AB do triângulo com seus dados: (A=120°, B-A= ?, B-C=12m, C=45°)",
+        correct: "05",
+        alt1:"12√6m",
+        alt2:"12√3m",
+        alt3:"8√6m",
+        alt4:"8√3m",
+        alt5:"4√6m"
+    },{
+        question:"Quantas casas decimais tem o número pi?",
+        correct: "03",
+        alt1:"Duas",
+        alt2:"Centenas",
+        alt3:"Infinitas",
+        alt4:"Vinte",
+        alt5:"Milhares"
+    }];
+    
+export const historyQuestions = [{ 
+        question:"Qual o noem do presidente do Brasil que ficou conhecido como Jango?",
+        correct: "05",
+        alt1:"Jânio Quadros",
+        alt2:"Jacinto Anjos",
+        alt3:"Getúlio Vargas",
+        alt4:"João Figueiredo",
+        alt5:"João Goulart"
+    },{
+        question:"Quais os principais autores do Barroco no Brasil?",
+        correct: "01",
+        alt1:"Gregório de Matos, Bento Teixeira e Manuel Botelho de Oliveira",
+        alt2:"Miguel de Cervantes, Gregório de Matos e Danthe Alighieri",
+        alt3:"Padre Antônio Vieira, Padre Manuel de Melo e Gregório de Matos",
+        alt4:"Castro Alves, Bento Teixeira e Manuel Botelho de Oliveira",
+        alt5:"Álvares de Azevedo, Gregório de Matos e Bento Teixeira"
+    },{
+        question:"Em que período da pré-história o fogo foi descoberto?",
+        correct: "02",
+        alt1:"Neolítico",
+        alt2:"Paleolítico",
+        alt3:"Idade dos Metais",
+        alt4:"Período da Pedra Polida",
+        alt5:"Idade Média"
+    }]
+export const englishQuestions = [{ 
+        question:"O que a palavra 'Legend' significa em Português?",
+        correct: "04",
+        alt1:"Legenda",
+        alt2:"Conto",
+        alt3:"História",
+        alt4:"Lenda",
+        alt5:"Legendário"
+    },{
+        question:"'It is six twenty' ou 'twenty past six'. Que horas são?",
+        correct: "02",
+        alt1:"12:06",
+        alt2:"6:20",
+        alt3:"2:20",
+        alt4:"6:02",
+        alt5:"12:20"
+    }]    
